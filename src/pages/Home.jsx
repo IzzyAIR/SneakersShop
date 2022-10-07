bdb8098e5772d5437aa0e4cbd9653dc4ede9ec90
IzzyAIR <izzycode2105@gmail.com>
2022-10-07 18:29:02 +0500

    fix bug 1.0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,71 @@
-import React from "react";
-import AppContext from "../context";
-import Card from "../components/Card/Card";
+import React from 'react';
+import AppContext from '../context';
+import Card from '../components/Card/Card';
 
 function Home() {
-  const {
-    items,
-    cardItems,
-    onAddToFavorite,
-    searchVal,
-    setSearchVal,
-    onChangeSearchInput,
-    onAddToCart,
-    isLoading,
-  } = React.useContext(AppContext);
+	const {
+		items,
+		cardItems,
+		onAddToFavorite,
+		searchVal,
+		setSearchVal,
+		onChangeSearchInput,
+		onAddToCart,
+		isLoading,
+	} = React.useContext(AppContext);
 
-  const rederItems = function () {
-    const filtered = items.filter((item) =>
-      item.title.toLowerCase().includes(searchVal.toLowerCase())
-    );
+	const rederItems = function () {
+		const filtered = items.filter((item) =>
+			item.title.toLowerCase().includes(searchVal.toLowerCase()),
+		);
 
-    return (isLoading ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] : filtered).map(
-      (item) => (
-        <Card
-          {...item}
-          isInCard={!!(cardItems || []).find((v) => v.id === item.id)}
-          key={item.id}
-          item={item}
-          loading={isLoading}
-          onPlus={(obj) => {
-            onAddToCart(obj);
-          }}
-          onFav={(obj) => {
-            onAddToFavorite(obj);
-          }}
-        />
-      )
-    );
-  };
-  return (
-    <div className="main content p-40">
-      <div className="d-flex align-center mb-40 justify-between">
-        <h1 className="title">
-          {searchVal ? `Поиск по запросу: ${searchVal}` : "Все кроссовки"}
-        </h1>
+		return (isLoading ? [...new Array(10)].map((val) => (val = 1)) : filtered).map((item) => (
+			<Card
+				{...item}
+				isInCard={!!(cardItems || []).find((v) => v.id === item.id)}
+				key={item.id}
+				item={item}
+				loading={isLoading}
+				onPlus={(obj) => {
+					onAddToCart(obj);
+				}}
+				onFav={(obj) => {
+					onAddToFavorite(obj);
+				}}
+			/>
+		));
+	};
+	return (
+		<div className='main content p-40'>
+			<div className='d-flex align-center mb-40 justify-between'>
+				<h1 className='title'>
+					{searchVal ? `Поиск по запросу: ${searchVal}` : 'Все кроссовки'}
+				</h1>
 
-        <div className="searchBlock d-flex">
-          <img src="/React-img/search.svg" alt="Search" />
-          <input
-            onChange={onChangeSearchInput}
-            value={searchVal}
-            placeholder="Поиск ..."
-          />
+				<div className='searchBlock d-flex'>
+					<img src='/React-img/search.svg' alt='Search' />
+					<input
+						onChange={onChangeSearchInput}
+						value={searchVal}
+						placeholder='Поиск ...'
+					/>
 
-          {searchVal && (
-            <img
-              onClick={() => {
-                setSearchVal("");
-              }}
-              className="cu-p clear"
-              src="/React-img/btnRemove.svg"
-              alt="Clear"
-            />
-          )}
-        </div>
-      </div>
+					{searchVal && (
+						<img
+							onClick={() => {
+								setSearchVal('');
+							}}
+							className='cu-p clear'
+							src='/React-img/btnRemove.svg'
+							alt='Clear'
+						/>
+					)}
+				</div>
+			</div>
 
-      <div className="cardBlock">{rederItems()}</div>
-    </div>
-  );
+			<div className='cardBlock'>{rederItems()}</div>
+		</div>
+	);
 }
 
 export default Home;
